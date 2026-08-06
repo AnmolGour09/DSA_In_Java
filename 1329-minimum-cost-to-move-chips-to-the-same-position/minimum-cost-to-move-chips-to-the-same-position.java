aca5d46f20df79bb61a31d0cc29c6af7f0949fce
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-08-07 02:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (14.49%) - LeetSync

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/minimum-cost-to-move-chips-to-the-same-position.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minCostToMoveChips(int[] a) {
+        int n=a.length;
+        int o=0;
+        int e=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2==0)
+            {
+                e++;
+            }
+            else{
+                o++;
+            }
+        }
+        
+        if(o<e)
+        {
+            return o;
+        }
+        else
+        {
+            return e;
+        }
+        
+    }
+}
