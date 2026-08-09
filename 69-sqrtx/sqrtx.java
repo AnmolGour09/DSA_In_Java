9e1972d28939efefc3b7e5dbb47e981fe3cab2bc
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-08-10 01:03:59 +0530

    Time: 1 ms (97.81%) | Memory: 42.3 MB (90.87%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,29 @@
+class Solution {
+
+        public static int mySqrt(int x)
+{
+    int s = 0;
+    int e = x;
+    int ans = 0;
+
+    while(s <= e)
+    {
+        int m=s+((e-s)/2);
+        if((long)m*m==x)
+        {
+            return m;
+        }
+        else if((long)m*m<x)
+        {
+            ans=m;
+            s=m+1;
+        }
+        else{
+            e=m-1;
+        }
+    }
+    return ans;
+}
+        
+    
+}
