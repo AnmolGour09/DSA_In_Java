f173f9e7cdcbd03a592d8e1901e264b1813c5edc
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-08-10 19:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (37.98%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPeakElement(int[] a) {
+        int n=a.length;
+        int s=0;
+        int e=n-1;
+        while(s<e)
+        {
+            int mid=s+(e-s)/2;
+            if(a[mid]>a[mid+1])
+            {
+                e=mid;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return e;
+    }
+}
