8550fe32796d3b22d1a1baaefefcd3b5f71edb24
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-08-15 19:32:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (66.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,16 +1,24 @@
 class Solution {
     public int search(int[] a, int t) {
-        int n=a.length;
-        int p=peak(a);
-        int f=binary(a,t,p);
-        if(f!=-1)
-        { 
-            return f;
-        }
-        
+    int p = 0;
 
-        return -1;        
+    for (int i = 1; i < a.length; i++) {
+        if (a[i] > a[p]) {
+            p = i;
+        }
     }
+    int f = binary(a, t, p);
+    if (f != -1) {
+        return f;
+    }
+
+    int s = binary2(a, t, p);
+    if (s != -1) {
+        return s;
+    }
+
+    return -1;
+}
 
     public static int peak(int[] a)
     {
@@ -32,36 +40,50 @@ class Solution {
 
     public static int binary(int[] a,int t, int p)
     {
-        int s = 0;
-        int e = a.length - 1;
-
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-
-            if (a[mid] == t) {
+        int n=a.length;
+        int s=0;
+        int e=p;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(a[mid]==t)
+            {
                 return mid;
             }
-
-            if (a[s] <= a[mid]) {
-
-                if (a[s] <= t && t < a[mid]) {
-                    e = mid - 1;
-                } else {
-                    s = mid + 1;
-                }
-
+            else if(a[mid]<t)
+            {
+                s=mid+1;
             }
-            else {
-
-                if (a[mid] < t && t <= a[e]) {
-                    s = mid + 1;
-                } else {
-                    e = mid - 1;
-                }
+            else{
+                e=mid-1;
             }
         }
         return -1;
     }
+
+    public static int binary2(int[] a,int t, int p)
+    {
+        int n=a.length;
+        int s=p+1;
+        int e=n-1;
+        while(s<=e)
+        {
+            int mid=s+(e-s)/2;
+            if(a[mid]==t)
+            {
+                return mid;
+            }
+            else if(a[mid]<t)
+            {
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return -1;
+    }
+
 
     
 }
