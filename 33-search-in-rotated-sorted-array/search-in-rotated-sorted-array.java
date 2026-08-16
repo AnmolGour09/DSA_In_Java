512798a2ac1fa8a31dfa10c6623d502f380f68a1
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-08-16 15:30:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (99.47%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,46 +1,51 @@
-class Solution {
-    public int search(int[] a, int t) {
-    int p = 0;
-
-    for (int i = 1; i < a.length; i++) {
-        if (a[i] > a[p]) {
-            p = i;
+class Solution 
+{
+    public int search(int[] a, int t) 
+    {
+        int p=peak(a);
+        int f=binary(a,p,t);
+        if(f!=-1)
+        {
+            return f;
         }
-    }
-    int f = binary(a, t, p);
-    if (f != -1) {
-        return f;
-    }
+        int s=binary2(a,p,t);
+        if(s!=-1)
+        {
+            return s;
+        }
 
-    int s = binary2(a, t, p);
-    if (s != -1) {
-        return s;
+        return -1;
     }
-
-    return -1;
-}
 
     public static int peak(int[] a)
     {
+        int n=a.length;
         int s=0;
-        int e=a.length-1;
-        while(s<e)
+        int e=n-1;
+        while(s<=e)
         {
             int mid=s+(e-s)/2;
-            if(a[mid]>a[mid+1])
+            if(mid<e && a[mid]>a[mid+1])// case 1:- [7>0] if a[mid]=7
             {
-                e=mid;
+                return mid;
+            }
+            if(mid>s && a[mid]<a[mid-1])// case 2:-[0<7] if a[mid]=0
+            {
+                return mid-1;
+            }
+            if(a[mid]<=a[s])  //[4,5,6,7,0,1,2] like a[mid]=0 and a[s]=7 then remove that extra right side of arrya because peak is lie in left side of the array
+            {
+                e=mid-1;
             }
             else{
                 s=mid+1;
             }
         }
-        return s;
+        return -1;
     }
 
-    public static int binary(int[] a,int t, int p)
+    public static int binary(int[] a, int p, int  t)
     {
-        int n=a.length;
         int s=0;
         int e=p;
         while(s<=e)
@@ -60,8 +65,7 @@ class Solution {
         }
         return -1;
     }
-
-    public static int binary2(int[] a,int t, int p)
+    public static int binary2(int[] a, int p, int  t)
     {
         int n=a.length;
         int s=p+1;
@@ -83,7 +87,4 @@ class Solution {
         }
         return -1;
     }
-
-
-    
 }
