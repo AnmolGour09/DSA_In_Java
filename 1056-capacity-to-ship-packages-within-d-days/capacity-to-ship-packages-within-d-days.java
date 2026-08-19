7a7978f90b2b9fa78cb41b368433a34c62f16280
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-08-19 18:08:28 +0530

    Time: 11 ms (66.33%) | Memory: 50 MB (88.49%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int shipWithinDays(int[] a, int k) {
+        int s=0;
+        int e=0;
+        for(int i=0;i<a.length;i++)
+        {
+            s=Math.max(s,a[i]);
+            e+=a[i];
+        }
+
+        while(s<e)
+        {
+            int mid=s+(e-s)/2;
+            int sum=0;
+            int p=1;
+            for(int i=0;i<a.length;i++)
+            {
+                if(sum+a[i]>mid)
+                {
+                    sum=a[i];
+                    p++;
+                }
+                else{
+                    sum+=a[i];
+                }
+            }
+
+            if(p>k)
+            {
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+        }
+        return e;
+    }
+}
