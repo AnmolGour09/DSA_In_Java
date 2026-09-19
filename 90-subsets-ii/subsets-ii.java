e6643270d6b141080a9eaea32c35891b0d367dc8
ANMOL GOUR <166193074+AnmolGour09@users.noreply.github.com>
2026-09-20 02:48:03 +0530

    Time: 2 ms (99.75%) | Memory: 45.5 MB (10.51%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int s=0;
+        int e=0;
+
+        for (int i=0;i<nums.length;i++) {
+            s=0;
+            if(i>0&&nums[i]==nums[i-1])
+            {
+                s=e;
+            }
+            e=outer.size();
+            for (int j = s; j < e; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(nums[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+}
